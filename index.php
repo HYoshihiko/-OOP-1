09bc58b9971f75950a570b9543c2d80dfcec5cfd
HYoshihiko <yoshi429to3@gmail.com>
2020-08-22 19:33:05 +0900

    課題2 oop

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 // 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
 
-class Car{
+class Car
+{
     private $Name;
     private $Number;
     private $Color;
@@ -58,9 +59,43 @@ class Car{
     }
 
 
-
 }
 
+
+class  Taxi extends Car
+{
+    private $passenger;
+
+    public function Get_passenger()
+    {
+        return $this->passenger;
+    }
+
+    public function pickUp($Up_number)
+    {
+        $this->passenger += $Up_number;
+    }
+
+    public function infomation()
+    {
+        parent::infomation();
+        echo '乗車人数は' . $this->passenger . '人です。';
+    }
+
+    public function lower($off_number)
+    {
+        $this->passenger -= $off_number;
+        if ($this->passenger >= 0)
+        {
+            echo $off_number . '人降車しました。';
+            $this->passenger -= $off_number;
+        } else
+        {
+            echo '降車人数に誤りがあります';
+        }
+        
+    }
+}
 
 
 // 以下を実行した時にエラーがでないようにして下さい。
@@ -68,37 +103,24 @@ class Car{
 // 引数 : 車名、車体番号、カラー
 $car1 = new Car('ランクル', 100, 'black');
 
-// 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
+// 車の情報を表示
 echo $car1->infomation();
 echo '<br>';
+echo '<hr>';
 
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+echo $taxi1->infomation();
 echo '<br>';
 
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
+// 2人降車
+$taxi1->lower(2);
 echo '<br>';
 
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
+// 2人降車
+$taxi1->lower(2);
